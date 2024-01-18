0e138707ac85f1ea57f46b89a6ef54b5c0f9ade8
Lootmann <chore.machines.10260930@gmail.com>
2024-01-19 08:45:02 +0900

    finished?

--- utils/download_samples/setup.py
+++ utils/download_samples/setup.py
@@ -1,24 +1,13 @@
-from setuptools import setup
-
-install_requires = [
-    "beautifulsoup4",
-    "lxml",
-    "requests",
-]
-
-packages = [
-    "atc",
-]
-
-console_scripts = [
-    "atc=atc:main",
-]
+from setuptools import find_packages, setup
 
 setup(
     name="atc",
     version="0.0.0",
-    packages=packages,
-    install_requires=install_requires,
-    console_scripts=console_scripts,
-    entry_points={"console_scripts": console_scripts},
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4",
+        "lxml",
+        "requests",
+    ],
+    entry_points={"console_scripts": ["atc=atc:main"]},
 )
