b3553872196957f8880d93afaebe1a8ef839660d
Lootmann <chore.machines.10260930@gmail.com>
2024-01-20 22:41:42 +0900

    agc a

---
+++ atc/agc014/A/solve.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define repa(i, a, n) for (int i = a; (int)(i) < (n); (++i))
+#define rrep(i, n) for (int i = (n - 1); (int)i >= 0; --i)
+#define rrepa(i, a, n) for (int i = a; (int)i >= 0; --i)
+#define EACH(x, a) for (auto& x : a)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <class A> void read(vector<A>& v);
+template <class A, size_t S> void read(array<A, S>& a);
+template <class T> void read(T& x) {
+  cin >> x;
+}
+void read(double& d) {
+  string t;
+  read(t);
+  d = stod(t);
+}
+void read(long double& d) {
+  string t;
+  read(t);
+  d = stold(t);
+}
+template <class H, class... T> void read(H& h, T&... t) {
+  read(h);
+  read(t...);
+}
+template <class A> void read(vector<A>& x) {
+  EACH(a, x) read(a);
+}
+template <class A, size_t S> void read(array<A, S>& x) {
+  EACH(a, x) read(a);
+}
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  llint a, b, c;
+  read(a, b, c);
+
+  if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1) die(0);
+
+  int cnt{};
+  dump(a, b, c);
+  while (true) {
+    cnt++;
+    llint aa = (b + c) / 2;
+    llint bb = (c + a) / 2;
+    llint cc = (a + b) / 2;
+    a = aa, b = bb, c = cc;
+    dump(a, b, c);
+
+    if (a == b && b == c) die(-1);
+    if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1) break;
+  }
+
+  output(cnt);
+}
