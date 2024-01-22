ded66e4ea8cde3bf923fcbddb4b0cec39825546e
Lootmann <chore.machines.10260930@gmail.com>
2024-01-23 05:39:39 +0900

    abc081 c

---
+++ atc/abc081/C/solve.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n, k;
+  cin >> n >> k;
+
+  map<int, int> mp;
+  rep(_, n) {
+    int a;
+    cin >> a;
+    mp[a]++;
+  }
+
+  int diff = (int)mp.size() - k;
+  if (diff <= 0) die(0);
+
+  dump(diff);
+
+  vector<int> kinds;
+  for (auto key : mp) {
+    kinds.push_back(key.second);
+  }
+
+  sort(kinds.begin(), kinds.end());
+
+  int ans{};
+  rep(i, diff) {
+    ans += kinds[i];
+  }
+  output(ans);
+}
