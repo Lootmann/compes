c3740fd557c66b05b8433cf89bfb242f9ac2e2b8
Lootmann <chore.machines.10260930@gmail.com>
2024-01-23 02:50:43 +0900

    abc073 c

---
+++ atc/abc073/C/solve.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  map<int, int> paper;
+  rep(i, n) {
+    int a;
+    cin >> a;
+    if (paper.find(a) != paper.end()) {
+      paper[a] = 1 - paper[a];
+    } else {
+      paper[a] = 1;
+    }
+  }
+
+  dump(paper);
+
+  int cnt{};
+  for (auto key : paper) {
+    cnt += key.second;
+  }
+  output(cnt);
+}
