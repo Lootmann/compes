9563339eecda6bdf24ed714d78b52702d6622412
Lootmann <chore.machines.10260930@gmail.com>
2024-01-23 03:40:52 +0900

    abc159 d

---
+++ atc/abc159/D/solve.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+llint comb(llint n) {
+  return n * (n - 1) / 2;
+}
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  vector<llint> ai(n);
+  map<llint, llint> balls;
+  rep(i, n) {
+    int a;
+    cin >> a;
+    balls[a]++;
+    ai[i] = a;
+  }
+
+  llint total{};
+  for (auto key : balls) {
+    total += comb(key.second);
+  }
+
+  rep(i, n) {
+    int idx = ai[i];
+    llint num = balls[idx];
+    output(total - comb(num) + comb(num - 1));
+  }
+}
