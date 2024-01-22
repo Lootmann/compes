59ae0685503cd5278cfa6434ad3df614a08446e6
Lootmann <chore.machines.10260930@gmail.com>
2024-01-22 11:31:47 +0900

    yahooprocon2019 qual b

---
+++ atc/yahoo-procon2019-qual/B/solve.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define repa(i, a, n) for (int i = a; (int)i < n; ++i)
+#define rrep(i, n) for (int i = n; (int)i >= 0; --i)
+#define rrepa(i, a, n) for (int i = a; (int)i >= n; --i)
+#define EACH(x, a) for (auto& x : a)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <class A> void read(vector<A>& v);
+template <class A, size_t S> void read(array<A, S>& a);
+template <class T> void read(T& x) {
+  cin >> x;
+}
+void read(double& d) {
+  string t;
+  read(t);
+  d = stod(t);
+}
+void read(long double& d) {
+  string t;
+  read(t);
+  d = stold(t);
+}
+template <class H, class... T> void read(H& h, T&... t) {
+  read(h);
+  read(t...);
+}
+template <class A> void read(vector<A>& x) {
+  EACH(a, x) read(a);
+}
+template <class A, size_t S> void read(array<A, S>& x) {
+  EACH(a, x) read(a);
+}
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+using P = pair<int, int>;
+
+int main() {
+  FastIO;
+  vector<P> vp(3);
+  rep(i, 3) {
+    int a, b;
+    read(a, b);
+    vp[i] = make_pair(a, b);
+  }
+  sort(vp.begin(), vp.end());
+
+  do {
+    P a = vp[0];
+    P b = vp[1];
+    P c = vp[2];
+
+    bool can_path = a.first == b.first && b.second == c.first;
+    can_path |= a.first == b.first && b.second == c.second;
+    can_path |= a.second == b.first && b.second == c.first;
+    can_path |= a.second == b.first && b.second == c.second;
+    if (can_path) die("YES");
+
+  } while (next_permutation(vp.begin(), vp.end()));
+
+  output("NO");
+}
