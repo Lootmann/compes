5f2e72950a7ec7abcba51940475e1d87767bb5f1
Lootmann <chore.machines.10260930@gmail.com>
2024-01-23 06:55:43 +0900

    abc090 c

---
+++ atc/abc090/C/solve.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+
+  llint n, m;
+  cin >> n >> m;
+
+  if (n == 1) {
+    if (m == 1) die(1);
+    if (m == 2) die(0);
+    die(m - 2);
+  }
+  if (m == 1) {
+    if (n == 1) die(1);
+    if (n == 2) die(0);
+    die(n - 2);
+  }
+
+  output((n - 2) * (m - 2));
+}
