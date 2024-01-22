f9b99d11cb05395ce5ff5378da7c77e1510ea2a2
Lootmann <chore.machines.10260930@gmail.com>
2024-01-22 12:15:01 +0900

    abc205 c - me

---
+++ atc/abc205/C/solve.cc
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define repa(i, a, n) for (int i = a; (int)i < n; ++i)
+#define rrep(i, n) for (int i = n; (int)i >= 0; --i)
+#define rrepa(i, a, n) for (int i = a; (int)i >= n; --i)
+#define EACH(x, a) for (auto& x : a)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <class A> void read(vector<A>& v);
+template <class A, size_t S> void read(array<A, S>& a);
+template <class T> void read(T& x) {
+  cin >> x;
+}
+void read(double& d) {
+  string t;
+  read(t);
+  d = stod(t);
+}
+void read(long double& d) {
+  string t;
+  read(t);
+  d = stold(t);
+}
+template <class H, class... T> void read(H& h, T&... t) {
+  read(h);
+  read(t...);
+}
+template <class A> void read(vector<A>& x) {
+  EACH(a, x) read(a);
+}
+template <class A, size_t S> void read(array<A, S>& x) {
+  EACH(a, x) read(a);
+}
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+void out(llint a, llint b) {
+  if (a > b) die(">");
+  if (a == b) die("=");
+  die("<");
+}
+
+int main() {
+  FastIO;
+  llint a, b, c;
+  read(a, b, c);
+
+  if (c % 2 == 0) {
+    a = abs(a), b = abs(b);
+    out(a, b);
+  }
+
+  if (a == b) die("=");
+
+  // a|b = 0
+  if (a == 0 && b == 0) die("=");
+
+  if (a == 0) {
+    if (a < b) die("<");
+    if (a > b) die(">");
+  }
+
+  if (b == 0) {
+    if (a > b) die(">");
+    if (a < b) die("<");
+  }
+
+  // + +
+  if (a > 0 && b > 0) {
+    out(a, b);
+  }
+
+  // + -
+  if (a > 0 && b < 0) {
+    die(">");
+  }
+
+  // - +
+  if (a < 0 && b > 0) {
+    die("<");
+  }
+
+  // - -
+  if (a < 0 && b < 0) {
+    if (a < b) die(">");
+    if (a > b) die("<");
+  }
+}
