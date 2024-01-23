bd9edeef62324a91ae73846951f88c7d47a4276d
Lootmann <chore.machines.10260930@gmail.com>
2024-01-24 01:27:12 +0900

    abc215 abc

---
+++ atc/abc215/B/solve.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+
+  llint n;
+  cin >> n;
+
+  vector<llint> vl;
+  llint two{};
+
+  for (int k = 1; k <= 60; ++k) {
+    if (two == 0)
+      two = 1;
+    else
+      two *= 2;
+    vl.push_back(two);
+  }
+
+  int ans{};
+  rep(i, 60) {
+    if (vl[i] <= n) {
+      ans = i;
+    }
+  }
+
+  output(ans);
+}
---
+++ atc/abc215/C/solve.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  string s;
+  int k;
+  cin >> s >> k;
+
+  sort(s.begin(), s.end());
+
+  string ans{};
+  do {
+    ans = s;
+    k--;
+    if (k == 0) break;
+  } while (next_permutation(s.begin(), s.end()));
+  output(ans);
+}
