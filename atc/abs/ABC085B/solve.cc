ca99e4726446a4f9508a43704b07dfd8cd3bf4e6
Lootmann <chore.machines.10260930@gmail.com>
2024-01-26 06:23:01 +0900

    abc085 b

---
+++ atc/abs/ABC085B/solve.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define INTi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  map<int, int> mp;
+  rep(i, n) {
+    int d;
+    cin >> d;
+    mp[d]++;
+  }
+  output(mp.size());
+}
