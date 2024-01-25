dced7f57e29b048cf97d86fd46f1325a685b81fd
Lootmann <chore.machines.10260930@gmail.com>
2024-01-26 05:58:18 +0900

    itp109c

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -27,32 +27,12 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
-void solve(int n) {
-  vector<double> si(n);
-  rep(i, n) cin >> si[i];
-
-  double average{};
-  rep(i, n) {
-    average += si[i];
-  }
-  average /= n;
-
-  double variance{};
-  rep(i, n) {
-    variance += powl((si[i] - average), 2);
-  }
-  double std = sqrt(variance / n);
-
-  cout << fixed << setprecision(8);
-  output(std);
-}
-
 int main() {
   FastIO;
-  while (true) {
-    int t;
-    cin >> t;
-    if (t == 0) return 0;
-    solve(t);
-  }
+
+  double x1, y1, x2, y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+
+  cout << fixed << setprecision(12);
+  cout << sqrt(pow(y1 - y2, 2) + pow(x1 - x2, 2)) << endl;
 }
