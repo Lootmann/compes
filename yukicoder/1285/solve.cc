268893d90d3e1f7a295a3cbab10af217a49acf50
Lootmann <chore.machines.10260930@gmail.com>
2024-01-25 12:00:57 +0900

    1285

---
+++ yukicoder/1285/solve.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; (int)i < n; ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(itr) (itr).begin(), (itr).end()
+#define INTi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  vector<int> ai(n);
+  rep(i, n) cin >> ai[i];
+
+  sort(all(ai));
+  dump(ai);
+  rep(i, n - 1) {
+    if (ai[i] + 1 == ai[i + 1]) {
+      die(2);
+    }
+  }
+  output(1);
+}
