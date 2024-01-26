cbd2298126bbf61c694d1e4fcd12fc2ec5e8d9ce
Lootmann <chore.machines.10260930@gmail.com>
2024-01-27 06:36:52 +0900

    aoj itp18c

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -27,21 +27,19 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
-void out(char ch) {
-  if ('a' <= ch && ch <= 'z') {
-    cout << (char)(ch - 'a' + 'A');
-  } else if ('A' <= ch && ch <= 'Z') {
-    cout << (char)(ch - 'A' + 'a');
-  } else {
-    cout << ch;
+int digit_sum(string x) {
+  int sum{};
+  for (auto ch : x) {
+    sum += ch - '0';
   }
+  return sum;
 }
 
 int main() {
   FastIO;
-  string s;
-  getline(cin, s);
-
-  for (auto ch : s) out(ch);
-  cout << endl;
+  string x;
+  while (cin >> x) {
+    if (x == "0") break;
+    cout << digit_sum(x) << '\n';
+  }
 }
