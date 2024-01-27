680b469ee639a4ff9e5c23fd28ca9167c9105235
Lootmann <chore.machines.10260930@gmail.com>
2024-01-27 10:45:17 +0900

    aoj itp21d

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -27,37 +27,86 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
+namespace me {
+
+struct vector {
+  int* m_array;
+  int m_idx;
+  int m_size;
+
+  vector() : m_idx{0}, m_size(10) { m_array = new int[m_size]; }
+  vector(int size_) : m_idx{0}, m_size(size_) { m_array = new int[size_]; }
+  ~vector() { delete[] m_array; }
+
+  void pushBack(int num) {
+    if (m_idx >= m_size) resize();
+    m_array[m_idx] = num;
+    m_idx++;
+  }
+
+  void resize() {
+    int* tmp = new int[m_size * 2];
+    for (int i = 0; i < m_size; ++i) {
+      tmp[i] = m_array[i];
+    }
+    m_size *= 2;
+    m_array = tmp;
+  }
+
+  void dumps() {
+    if (m_idx == 0) {
+      cout << '\n';
+      return;
+    }
+
+    for (int i = 0; i < m_idx; ++i) {
+      cout << m_array[i] << (i == m_idx - 1 ? '\n' : ' ');
+    }
+  }
+
+  void clear() {
+    m_array = new int[m_size];
+    m_idx = 0;
+  }
+};
+
+struct vector2 {
+  int m_size;
+  me::vector* vectors;
+  vector2(int size) : m_size(size) { vectors = new vector[size]; }
+  ~vector2() { delete[] vectors; }
+
+  void pushBack(int t, int x) { vectors[t].pushBack(x); }
+  void dumps(int t) { vectors[t].dumps(); }
+  void clear(int t) { vectors[t].clear(); }
+};
+
+};  // namespace me
+
 int main() {
   FastIO;
-  deque<int> que;
 
-  int n;
-  cin >> n;
+  int n, q;
+  cin >> n >> q;
 
-  rep(_, n) {
+  me::vector2 v(n);
+
+  rep(_, q) {
     int op;
     cin >> op;
 
     if (op == 0) {
-      int d, x;
-      cin >> d >> x;
-      if (d == 0)
-        que.push_front(x);
-      else
-        que.push_back(x);
-
+      int t, x;
+      cin >> t >> x;
+      v.pushBack(t, x);
     } else if (op == 1) {
-      int p;
-      cin >> p;
-      output(que[p]);
-
+      int t;
+      cin >> t;
+      v.dumps(t);
     } else {
-      int d;
-      cin >> d;
-      if (d == 0)
-        que.pop_front();
-      else
-        que.pop_back();
+      int t;
+      cin >> t;
+      v.clear(t);
     }
   }
 }
