cbb3a8037cc2c3982d78826085740c1c77f1451a
Lootmann <chore.machines.10260930@gmail.com>
2024-01-27 10:07:13 +0900

    aoj itp21a

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -27,81 +27,77 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
-struct Dice {
-  int one, two, three, four, five, six;
-  Dice() {}
-  Dice(int o, int tw, int th, int fo, int fi, int s)
-      : one(o), two(tw), three(th), four(fo), five(fi), six(s) {}
+namespace me {
+struct vector {
+  int* array;
+  int idx;
+  int size;
 
-  // direction
-  void move(char d) {
-    if (d == 'N') {
-      int tmp = one;
-      one = two;
-      two = six;
-      six = five;
-      five = tmp;
-    } else if (d == 'S') {
-      int tmp = one;
-      one = five;
-      five = six;
-      six = two;
-      two = tmp;
-    } else if (d == 'E') {
-      int tmp = one;
-      one = four;
-      four = six;
-      six = three;
-      three = tmp;
-    } else {
-      int tmp = one;
-      one = three;
-      three = six;
-      six = four;
-      four = tmp;
+  vector() : idx(0), size(10) {
+    array = new int[size];
+    for (int i = 0; i < size; ++i) {
+      array[i] = 0;
     }
   }
 
-  // xxxxxx
-  int die_number() {
-    return 100000 * one + 10000 * two + 1000 * three + 100 * four + 10 * five +
-           six;
+  vector(int size_) : idx(0), size(size_) {
+    array = new int[size_];
+    for (int i = 0; i < size_; ++i) {
+      array[i] = 0;
+    }
+  }
+  ~vector() { delete[] array; }
+
+  void pushBack(int x) {
+    if (size <= idx) {
+      resize();
+    }
+
+    array[idx++] = x;
+  }
+
+  int randomAccess(int p) {
+    assert(size >= p);
+    return array[p];
+  }
+
+  bool popBack() {
+    idx--;
+    return true;
+  }
+
+  bool resize() {
+    size *= 2;
+    int* tmp = new int[size];
+    for (int i = 0; i < idx; ++i) {
+      tmp[i] = array[i];
+    }
+    array = tmp;
+    return true;
   }
 };
-
-Dice input() {
-  int one, two, three, four, five, six;
-  cin >> one >> two >> three >> four >> five >> six;
-  return Dice(one, two, three, four, five, six);
-}
-
-bool is_same_dice(Dice d1, Dice d2) {
-  for (auto direction : "NNNNWNNNWNNNENNNENNNWNNN") {
-    d1.move(direction);
-    if (d1.die_number() == d2.die_number()) {
-      return true;
-    }
-  }
-  return false;
-}
+};  // namespace me
 
 int main() {
   FastIO;
   int n;
   cin >> n;
 
-  vector<Dice> dices;
+  me::vector v(n);
   rep(_, n) {
-    Dice d = input();
-    dices.push_back(d);
-  }
+    int query;
+    cin >> query;
 
-  for (int i = 0; i < n; ++i) {
-    for (int j = i + 1; j < n; ++j) {
-      if (is_same_dice(dices[i], dices[j])) {
-        die("No");
-      }
+    if (query == 0) {
+      int x;
+      cin >> x;
+      v.pushBack(x);
+    } else if (query == 1) {
+      int p;
+      cin >> p;
+      output(v.randomAccess(p));
+    } else {
+      v.popBack();
     }
   }
-  output("Yes");
 }
