aa8a087adb0fa077430f6608c7d84b237230443f
Lootmann <chore.machines.10260930@gmail.com>
2024-01-27 13:17:35 +0900

    itp25b

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -26,23 +26,45 @@ template <typename T> bool chmin(T& a, const T& b) {
 }
 
 using llint = long long int;
-using P = pair<int, int>;
+using T = tuple<int, int, char, llint, string>;
+
+bool compare(const T& l, const T& r) {
+  if (get<0>(l) == get<0>(r)) {
+    if (get<1>(l) == get<1>(r)) {
+      if (get<2>(l) == get<2>(r)) {
+        if (get<3>(l) == get<3>(r)) {
+          return get<4>(l) < get<4>(r);
+        }
+        return get<3>(l) < get<3>(r);
+      }
+      return get<2>(l) < get<2>(r);
+    }
+    return get<1>(l) < get<1>(r);
+  }
+  return get<0>(l) < get<0>(r);
+}
 
 int main() {
   FastIO;
   int n;
   cin >> n;
 
-  vector<P> vp(n);
+  vector<T> vt(n);
+
   rep(i, n) {
-    int x, y;
-    cin >> x >> y;
-    vp[i] = make_pair(x, y);
+    int value, weight;
+    char id;
+    llint date;
+    string name;
+    cin >> value >> weight >> id >> date >> name;
+    vt[i] = tuple(value, weight, id, date, name);
   }
 
-  sort(vp.begin(), vp.end());
+  sort(vt.begin(), vt.end(), compare);
 
   rep(i, n) {
-    cout << vp[i].first << ' ' << vp[i].second << '\n';
+    auto item = vt[i];
+    cout << get<0>(item) << ' ' << get<1>(item) << ' ' << get<2>(item) << ' '
+         << get<3>(item) << ' ' << get<4>(item) << '\n';
   }
 }
