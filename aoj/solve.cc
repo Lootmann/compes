82889b99e09587c6e41a9607465d9d10eaf20eac
Lootmann <chore.machines.10260930@gmail.com>
2024-01-27 13:58:42 +0900

    itp26c

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -10,12 +10,12 @@ using namespace std;
 #define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
 #define rep(i, n) for (int i = 0; (int)i < n; ++i)
 #define output(msg) cout << (msg) << '\n'
-#define all(k) k.begin(), k.end()
 #define die(msg)         \
   do {                   \
     cout << msg << endl; \
     exit(0);             \
   } while (0)
+#define all(k) k.begin(), k.end()
 #define INFi 1 << 30
 #define INFll 1LL << 60
 
@@ -32,11 +32,21 @@ int main() {
   FastIO;
   int n;
   cin >> n;
-
   vector<int> ai(n);
-  rep(i, n) ai[i] = i + 1;
+  rep(i, n) cin >> ai[i];
 
-  do {
-    rep(i, n) cout << ai[i] << (i == n - 1 ? '\n' : ' ');
-  } while (next_permutation(all(ai));
+  int q;
+  cin >> q;
+  rep(_, q) {
+    int k;
+    cin >> k;
+
+    auto it = lower_bound(ai.begin(), ai.end(), k);
+    if (it != ai.end()) {
+      size_t pos = distance(ai.begin(), it);
+      output(pos);
+    } else {
+      output(n);
+    }
+  }
 }
