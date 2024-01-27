c72491fd90198a20c3f3437d7ee9b29c5cd38241
Lootmann <chore.machines.10260930@gmail.com>
2024-01-27 11:37:22 +0900

    aoj itp22a

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+
+#include <cwchar>
 using namespace std;
 
 #ifdef DEBUG_
@@ -27,70 +29,13 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
-namespace me {
-
-struct vector {
-  int* m_array;
-  int m_idx;
-  int m_size;
-
-  vector() : m_idx{0}, m_size(10) { m_array = new int[m_size]; }
-  vector(int size_) : m_idx{0}, m_size(size_) { m_array = new int[size_]; }
-  ~vector() { delete[] m_array; }
-
-  void pushBack(int num) {
-    if (m_idx >= m_size) resize();
-    m_array[m_idx] = num;
-    m_idx++;
-  }
-
-  void resize() {
-    int* tmp = new int[m_size * 2];
-    for (int i = 0; i < m_size; ++i) {
-      tmp[i] = m_array[i];
-    }
-    m_size *= 2;
-    m_array = tmp;
-  }
-
-  void dumps() {
-    if (m_idx == 0) {
-      cout << '\n';
-      return;
-    }
-
-    for (int i = 0; i < m_idx; ++i) {
-      cout << m_array[i] << (i == m_idx - 1 ? '\n' : ' ');
-    }
-  }
-
-  void clear() {
-    m_array = new int[m_size];
-    m_idx = 0;
-  }
-};
-
-struct vector2 {
-  int m_size;
-  me::vector* vectors;
-  vector2(int size) : m_size(size) { vectors = new vector[size]; }
-  ~vector2() { delete[] vectors; }
-
-  void pushBack(int t, int x) { vectors[t].pushBack(x); }
-  void dumps(int t) { vectors[t].dumps(); }
-  void clear(int t) { vectors[t].clear(); }
-};
-
-};  // namespace me
-
 int main() {
   FastIO;
 
   int n, q;
   cin >> n >> q;
 
-  me::vector2 v(n);
-
+  vector<stack<int>> vs(n);
   rep(_, q) {
     int op;
     cin >> op;
@@ -98,15 +43,17 @@ int main() {
     if (op == 0) {
       int t, x;
       cin >> t >> x;
-      v.pushBack(t, x);
+      vs[t].push(x);
     } else if (op == 1) {
       int t;
       cin >> t;
-      v.dumps(t);
+      if (!vs[t].empty()) {
+        cout << vs[t].top() << '\n';
+      }
     } else {
       int t;
       cin >> t;
-      v.clear(t);
+      if (!vs[t].empty()) vs[t].pop();
     }
   }
 }
