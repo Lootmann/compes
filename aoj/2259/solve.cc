4c7f4e477cb0a77d38f0c32d15abb99439e6125c
Lootmann <chore.machines.10260930@gmail.com>
2024-01-29 04:25:56 +0900

    2259

---
+++ aoj/2259/solve.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+
+  int n, m;
+  cin >> n >> m;
+
+  int ans{};
+  rep(i, n) {
+    int cnt{};
+    rep(j, m) {
+      int res;
+      cin >> res;
+      cnt += res;
+    }
+    chmax(ans, cnt);
+  }
+  output(ans);
+}
