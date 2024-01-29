a587ba896a2f7f20acaaa29987c28140678a07de
Lootmann <chore.machines.10260930@gmail.com>
2024-01-29 09:54:52 +0900

    0010

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <math.h>
 using namespace std;
 
 #ifdef DEBUG_
@@ -28,36 +29,42 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
-bool is_prime(int n) {
-  if (n == 2) return true;
-  if (n % 2 == 0) return false;
-  if (n == 3) return true;
-  if (n % 3 == 0) return false;
+void solve() {
+  double x1, y1, x2, y2, x3, y3;
+  cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
-  for (int i = 3; i * i <= n; i += 2) {
-    if (n % i == 0) {
-      return false;
-    }
-  }
+  double x1_2 = x1 * x1, x2_2 = x2 * x2, x3_2 = x3 * x3;
+  double y1_2 = y1 * y1, y2_2 = y2 * y2, y3_2 = y3 * y3;
 
-  return true;
-}
+  double xy1 = x1_2 + y1_2;
+  double xy2 = x2_2 + y2_2;
+  double xy3 = x3_2 + y3_2;
 
-int count_prime(int n) {
-  int cnt{};
-  for (int i = 2; i <= n; ++i) {
-    if (is_prime(i)) {
-      cnt++;
-    }
-  }
-  return cnt;
+  double diff_x12 = x1 - x2;
+  double diff_x23 = x2 - x3;
+  double diff_x31 = x3 - x1;
+
+  double diff_y12 = y1 - y2;
+  double diff_y23 = y2 - y3;
+  double diff_y31 = y3 - y1;
+
+  double p = xy1 * diff_y23 + xy2 * diff_y31 + xy3 * diff_y12;
+  p /= (diff_x12 * diff_y23 - diff_x23 * diff_y12);
+  p /= 2;
+
+  double q = xy1 * diff_x23 + xy2 * diff_x31 + xy3 * diff_x12;
+  q /= (diff_x23 * diff_y12 - diff_x12 * diff_y23);
+  q /= 2;
+
+  double r = sqrtl(powl(x1 - p, 2) + powl(y1 - q, 2));
+
+  cout << p << ' ' << q << ' ' << r << '\n';
 }
 
 int main() {
   FastIO;
-
-  int n;
-  while (cin >> n) {
-    output(count_prime(n));
-  }
+  int t;
+  cin >> t;
+  cout << fixed << setprecision(3);
+  rep(_, t) solve();
 }
