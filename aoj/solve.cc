d610705ff37dace0e631888ed6ec2c0b29753e96
Lootmann <chore.machines.10260930@gmail.com>
2024-01-30 18:11:03 +0900

    alds11a

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -28,89 +28,35 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
-namespace me {
-template <typename T> struct Node {
-  Node* m_nxt;
-  int m_num;
-  Node() : m_nxt{nullptr}, m_num{0} {}
-  Node(int num) : m_nxt{nullptr}, m_num{num} {}
-};
+void out(const vector<int>& ai, int n) {
+  rep(i, n) {
+    cout << ai[i] << (i == n - 1 ? '\n' : ' ');
+  }
+}
 
-template <typename T> struct Stack {
-  Node<T>* m_dummy;
-  Stack() { m_dummy = new Node<T>(-1); }
-  ~Stack() {
-    while (m_dummy->m_nxt != nullptr) {
-      Node<T>* tmp = m_dummy->m_nxt;
-      m_dummy->m_nxt = m_dummy->m_nxt->m_nxt;
-      delete tmp;
+void insertionSort(vector<int>& ai, int n) {
+  out(ai, n);
+
+  for (int i = 1; i < n; ++i) {
+    int v = ai[i];
+    int j = i - 1;
+    while (j >= 0 && ai[j] > v) {
+      ai[j + 1] = ai[j];
+      j--;
     }
-    delete m_dummy;
+    ai[j + 1] = v;
+
+    out(ai, n);
   }
-
-  void push(int num) {
-    Node<T>* new_node = new Node<T>(num);
-
-    if (is_empty()) {
-      m_dummy->m_nxt = new_node;
-    } else {
-      new_node->m_nxt = m_dummy->m_nxt;
-      m_dummy->m_nxt = new_node;
-    }
-  }
-
-  T pop() {
-    assert(!is_empty());
-    Node<T>* pop_node = m_dummy->m_nxt;
-    m_dummy->m_nxt = m_dummy->m_nxt->m_nxt;
-
-    int num = pop_node->m_num;
-    delete pop_node;
-    return num;
-  }
-
-  int top() {
-    assert(!is_empty());
-    return m_dummy->m_nxt->m_num;
-  }
-
-  bool is_empty() { return m_dummy->m_nxt == nullptr; }
-
-  void out() {
-    Node<T>* tmp = m_dummy->m_nxt;
-    while (tmp != nullptr) {
-      cerr << "[" << tmp->m_num << "]";
-      tmp = tmp->m_nxt;
-    }
-    cerr << '\n';
-  }
-};
-}  // namespace me
+}
 
 int main() {
   FastIO;
-  me::Stack<int> st;
+  int n;
+  cin >> n;
 
-  string s;
-  while (cin >> s) {
-    if (s == "+") {
-      int left = st.pop();
-      int right = st.pop();
-      st.push(left + right);
-    } else if (s == "-") {
-      int left = st.pop();
-      int right = st.pop();
-      st.push(right - left);
-    } else if (s == "*") {
-      int left = st.pop();
-      int right = st.pop();
-      st.push(left * right);
-    } else {
-      st.push(stoi(s));
-    }
-    st.out();
-  }
+  vector<int> ai(n);
+  rep(i, n) cin >> ai[i];
 
-  // st.out();
-  cout << st.top() << '\n';
+  insertionSort(ai, n);
 }
