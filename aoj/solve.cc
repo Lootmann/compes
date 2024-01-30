71a837e824bf274799c61a51d96822f7c25869b2
Lootmann <chore.machines.10260930@gmail.com>
2024-01-30 19:51:21 +0900

    alds12d

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -28,35 +28,24 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
-void out(const vector<int>& ai, int n) {
-  rep(i, n) {
-    cout << ai[i] << (i == n - 1 ? '\n' : ' ');
-  }
-}
-
-void insertionSort(vector<int>& ai, int n) {
-  out(ai, n);
-
-  for (int i = 1; i < n; ++i) {
-    int v = ai[i];
-    int j = i - 1;
-    while (j >= 0 && ai[j] > v) {
-      ai[j + 1] = ai[j];
-      j--;
-    }
-    ai[j + 1] = v;
-
-    out(ai, n);
-  }
-}
-
 int main() {
   FastIO;
   int n;
   cin >> n;
 
-  vector<int> ai(n);
-  rep(i, n) cin >> ai[i];
+  vector<int> ri(n);
+  rep(i, n) cin >> ri[i];
 
-  insertionSort(ai, n);
+  int i{};
+  int minv{INFi}, ans{ri[1] - ri[0]};
+  rep(j, n) {
+    if (chmin(minv, ri[j])) {
+      i = j;
+    }
+
+    if (j > i) {
+      chmax(ans, ri[j] - minv);
+    }
+  }
+  output(ans);
 }
