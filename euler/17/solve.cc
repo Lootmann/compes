9b18cc99152c759d695b39dcab0db671ed699c4c
Lootmann <chore.machines.10260930@gmail.com>
2024-01-31 19:42:17 +0900

    euler 17

---
+++ euler/17/solve.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int d(int n) {
+  int sum{};
+  for (int i = 1; i <= n / 2; ++i) {
+    if (n % i == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+int main() {
+  FastIO;
+
+  int n;
+  cin >> n;
+
+  int sum{};
+  for (int i = 1; i < n; ++i) {
+    int a = d(i);
+    int b = d(a);
+
+    if (i == b && i != a) {
+      dump(i);
+      sum += i;
+    }
+  }
+
+  output(sum);
+}
