899182314275839250b4dc61e0d18e7530562ff3
Lootmann <chore.machines.10260930@gmail.com>
2024-02-01 05:01:36 +0900

    euler 48

---
+++ euler/48/solve.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+llint MOD(llint n) {
+  return n % 10000000000;
+}
+
+int main() {
+  FastIO;
+
+  llint sum{};
+  for (llint i = 1; i <= 1000; ++i) {
+    llint num{1};
+    rep(_, i) num = MOD(MOD(num) * i);
+    sum = MOD(sum + num);
+  }
+  output(MOD(sum));
+}
