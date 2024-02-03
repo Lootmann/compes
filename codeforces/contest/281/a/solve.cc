ff80df8c6e94d8b5c4c3b7add2dfe1d0cd558908
Lootmann <chore.machines.10260930@gmail.com>
2024-02-04 07:23:29 +0900

    cf 281 a

---
+++ codeforces/contest/281/a/solve.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  string s;
+  cin >> s;
+
+  rep(i, (int)s.size()) {
+    if (i == 0 && 'a' <= s[0] && s[0] <= 'z') {
+      cout << char(s[0] - 'a' + 'A');
+    } else {
+      cout << s[i];
+    }
+  }
+  cout << '\n';
+}
