8d2029887e4464043198a10cde7df4c11fd6cd7c
Lootmann <chore.machines.10260930@gmail.com>
2024-02-05 08:26:03 +0900

    yuki 2560

--- yukicoder/2560/solve.cc
+++ yukicoder/2560/solve.cc
@@ -9,9 +9,6 @@ using namespace std;
 
 #define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
 #define rep(i, n) for (int i = 0; (int)i < n; ++i)
-#define repa(i, a, n) for (int i = a; (int)(i) < (n); (++i))
-#define rrep(i, n) for (int i = (n - 1); (int)i >= 0; --i)
-#define rrepa(i, a, n) for (int i = a; (int)i >= 0; --i)
 #define EACH(x, a) for (auto& x : a)
 #define output(msg) cout << (msg) << '\n'
 #define die(msg)         \
@@ -20,31 +17,6 @@ using namespace std;
     exit(0);             \
   } while (0)
 
-template <class A> void read(vector<A>& v);
-template <class A, size_t S> void read(array<A, S>& a);
-template <class T> void read(T& x) {
-  cin >> x;
-}
-void read(double& d) {
-  string t;
-  read(t);
-  d = stod(t);
-}
-void read(long double& d) {
-  string t;
-  read(t);
-  d = stold(t);
-}
-template <class H, class... T> void read(H& h, T&... t) {
-  read(h);
-  read(t...);
-}
-template <class A> void read(vector<A>& x) {
-  EACH(a, x) read(a);
-}
-template <class A, size_t S> void read(array<A, S>& x) {
-  EACH(a, x) read(a);
-}
 template <typename T> bool chmax(T& a, const T& b) {
   return ((a < b) ? (a = b, true) : (false));
 }
@@ -56,14 +28,16 @@ using llint = long long int;
 
 void solve() {
   llint n, x;
-  read(n, x);
+  cin >> n >> x;
 
   llint sum = n * (n + 1) / 2;
   if (sum > x) {
     output(-1);
   } else {
     llint diff = x - sum;
-    repa(i, 1, n) cout << i << ' ';
+    for (int i = 1; i < n; ++i) {
+      cout << i << ' ';
+    }
     cout << n + diff << '\n';
   }
 }
@@ -71,6 +45,6 @@ void solve() {
 int main() {
   FastIO;
   int t;
-  read(t);
+  cin >> t;
   rep(_, t) solve();
 }
