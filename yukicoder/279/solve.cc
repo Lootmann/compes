db6a6a8fd0235ab6fabbfef3cd306904df3eaa7f
Lootmann <chore.machines.10260930@gmail.com>
2024-02-05 17:43:18 +0900

    yuki 279

---
+++ yukicoder/279/solve.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  string s;
+  cin >> s;
+
+  int t{}, r{}, e{};
+  for (auto ch : s) {
+    if (ch == 't') t++;
+    if (ch == 'r') r++;
+    if (ch == 'e') e++;
+  }
+
+  output(min(t, min(r, e / 2)));
+}
