b397a3bf7e673594097a303c2db5341399db9ead
Lootmann <chore.machines.10260930@gmail.com>
2024-02-05 19:10:18 +0900

    yuki 418

---
+++ yukicoder/418/solve.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  string s;
+  cin >> s;
+
+  string t;
+  for (auto ch : s) {
+    if (ch != '-') t.push_back(ch);
+  }
+
+  int cnt{}, n = (int)t.size();
+  rep(i, n - 2) {
+    if (t[i] == 'm' && t[i + 1] == 'i' && t[i + 2] == 'n') {
+      cnt++;
+    }
+  }
+  output(cnt);
+}
