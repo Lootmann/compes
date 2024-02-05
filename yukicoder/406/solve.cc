f1e70b39894dfac333d9a13c0eaaca800c1970fa
Lootmann <chore.machines.10260930@gmail.com>
2024-02-05 18:51:22 +0900

    yuki 406

---
+++ yukicoder/406/solve.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  vector<int> kamo(n);
+  rep(i, n) cin >> kamo[i];
+  sort(all(kamo));
+
+  int diff = abs(kamo[0] - kamo[1]);
+  rep(i, n - 1) {
+    int d = abs(kamo[i] - kamo[i + 1]);
+    if (diff != d || d == 0) {
+      die("NO");
+    }
+  }
+
+  output("YES");
+}
