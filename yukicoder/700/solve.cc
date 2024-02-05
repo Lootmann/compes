6ab041205dd4ae43016e71e9c926492b03f8133f
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 07:02:29 +0900

    yuki 700

---
+++ yukicoder/700/solve.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int H, W;
+  cin >> H >> W;
+
+  vector<string> pic(H);
+  rep(i, H) {
+    string line;
+    cin >> line;
+    pic[i] = line;
+  }
+
+  rep(h, H) {
+    rep(w, W - 3) {
+      if (pic[h].substr(w, 4) == "LOVE") {
+        die("YES");
+      }
+    }
+  }
+
+  output("NO");
+}
