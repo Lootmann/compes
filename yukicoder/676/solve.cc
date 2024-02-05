2e02726ba0d5792fa75f8c8334cb5f58b2c457b3
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 06:54:08 +0900

    yuki 676

---
+++ yukicoder/676/solve.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  string s;
+  cin >> s;
+  for (auto ch : s) {
+    if (ch == 'I' || ch == 'l')
+      cout << '1';
+    else if (ch == 'O' || ch == 'o')
+      cout << '0';
+    else
+      cout << ch;
+  }
+  cout << endl;
+}
