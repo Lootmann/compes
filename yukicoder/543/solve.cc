a0f329f7836daa3f94f3e1bcf658bcff2e282f29
Lootmann <chore.machines.10260930@gmail.com>
2024-02-05 19:51:17 +0900

    yuki 543

---
+++ yukicoder/543/solve.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  char t, f;
+  cin >> t >> f;
+
+  if (t == 'T' && f == 'T') {
+    cout << "T T" << endl;
+  } else if (t == 'F' && f == 'F') {
+    cout << "F F" << endl;
+  } else if (t == 'T') {
+    cout << "F T" << endl;
+  } else {
+    cout << "T F" << endl;
+  }
+}
