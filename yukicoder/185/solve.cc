f3a0ed59770f2e491e695d24776c6fb4c2d0af58
Lootmann <chore.machines.10260930@gmail.com>
2024-02-05 16:19:34 +0900

    yuki 185

---
+++ yukicoder/185/solve.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  set<int> st;
+  rep(_, n) {
+    int x, y;
+    cin >> x >> y;
+    st.insert(y - x);
+  }
+
+  if (st.size() == 1) {
+    int num;
+    for (auto key : st) num = key;
+    if (num > 0) die(num);
+  }
+
+  output(-1);
+}
