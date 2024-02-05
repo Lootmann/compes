66787156088216da1cec3f4dd1aa189d9bb97be2
Lootmann <chore.machines.10260930@gmail.com>
2024-02-05 09:56:59 +0900

    yuki 48

---
+++ yukicoder/48/solve.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+
+  int x, y, l;
+  cin >> x >> y >> l;
+
+  int ans{};
+
+  if (y < 0)
+    ans = 2;
+  else if (x != 0)
+    ans = 1;
+
+  x = abs(x), y = abs(y);
+
+  ans += (y + l - 1) / l;
+  ans += (x + l - 1) / l;
+
+  output(ans);
+}
