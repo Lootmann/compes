771dfff94826807567884e333e14206e39b74653
Lootmann <chore.machines.10260930@gmail.com>
2024-02-05 12:23:47 +0900

    yuki 79

---
+++ yukicoder/79/solve.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  vector<int> li(n);
+  rep(i, n) cin >> li[i];
+
+  vector<int> votes(7, 0);
+  rep(i, n) {
+    votes[li[i]]++;
+  }
+
+  int cnt{}, max_idx{};
+  rep(i, 7) {
+    if (cnt <= votes[i]) {
+      cnt = votes[i];
+      max_idx = i;
+    }
+  }
+  output(max_idx);
+}
