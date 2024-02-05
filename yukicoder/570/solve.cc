0554541cd6b71b5bcffdaaf6c777b0da42236dad
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 05:58:08 +0900

    yuki 570

---
+++ yukicoder/570/solve.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  vector<pair<int, char>> hi(3);
+  rep(i, 3) {
+    int h;
+    cin >> h;
+    hi[i] = make_pair(h, (char)('A' + i));
+  }
+  sort(hi.rbegin(), hi.rend());
+
+  rep(i, 3) {
+    output(hi[i].second);
+  }
+}
