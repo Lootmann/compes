48f7acf984df95d2a99da21994e08284a618aa86
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 15:26:45 +0900

    yuki 1205

---
+++ yukicoder/1205/solve.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n, m;
+  cin >> n >> m;
+
+  int cur_t{0}, cur_p{0};
+  rep(_, m) {
+    int t, p;
+    cin >> t >> p;
+
+    int diff_t = t - cur_t;
+    int diff_p = abs(p - cur_p);
+    cur_t = t;
+    cur_p = p;
+
+    if (diff_t < diff_p) {
+      die("No");
+    }
+  }
+
+  output("Yes");
+}
