18d06bf621ca4006028c8107d2955cb7bfb67cdf
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 18:43:14 +0900

    yuki 791

---
+++ yukicoder/791/solve.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  string s;
+  cin >> s;
+
+  if (s == "1") die(-1);
+  if (s.size() == 1) die(-1);
+
+  rep(i, (int)s.size()) {
+    if (i == 0) {
+      if (s[i] != '1') die(-1);
+    } else {
+      if (s[i] != '3') die(-1);
+    }
+  }
+
+  out((int)s.size() - 1);
+}
