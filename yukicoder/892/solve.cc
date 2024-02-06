0aa2e6b09c4ff8ba1a7531cc100ad459e0087430
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 20:00:10 +0900

    yuki 892

---
+++ yukicoder/892/solve.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int odd{};
+  rep(_, 3) {
+    int a, b;
+    cin >> a >> b;
+
+    if (a % 2 == 1) odd++;
+  }
+
+  if (odd == 1 || odd == 3)
+    out(":-(");
+  else
+    out(":-)");
+}
