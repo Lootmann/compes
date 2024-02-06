af0bbf7f96521ad0904e15b5b64ecb02167291e3
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 15:34:34 +0900

    yuki 1131

---
+++ yukicoder/1131/solve.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  vector<double> xi(n);
+  rep(i, n) cin >> xi[i];
+
+  double ave = (double)accumulate(xi.begin(), xi.end(), 0) / n;
+
+  rep(i, n) {
+    double di = 50 - (ave - xi[i]) / 2;
+    output((int)di);
+  }
+}
