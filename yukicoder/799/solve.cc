7aa52c79431ae9eb618313c108ed3ce628e37cc6
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 18:58:49 +0900

    yuki 799

---
+++ yukicoder/799/solve.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  int sum = (b - a + 1) * (d - c + 1);
+  int cnt{};
+  for (int i = a; i <= b; ++i) {
+    for (int j = c; j <= d; ++j) {
+      if (i == j) cnt++;
+    }
+  }
+
+  dump(sum - cnt);
+  out(sum - cnt);
+}
