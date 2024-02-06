a39a906f3376c2b0a76a0f77a5b0e9b469792bbf
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 17:10:18 +0900

    yuki 998

---
+++ yukicoder/998/solve.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  vector<int> ni(4);
+  rep(i, 4) cin >> ni[i];
+  sort(all(ni));
+
+  bool is_series{true};
+  rep(i, 3) {
+    if (ni[i] + 1 != ni[i + 1]) {
+      is_series = false;
+    }
+  }
+
+  if (is_series)
+    out("Yes");
+  else
+    out("No");
+}
