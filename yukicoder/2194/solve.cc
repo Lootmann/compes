8808b5f7ba3a93d135cf8cb24289525b797fc253
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 10:09:40 +0900

    yuki 2194

--- yukicoder/2194/solve.cc
+++ yukicoder/2194/solve.cc
@@ -8,43 +8,17 @@ using namespace std;
 #endif
 
 #define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
-#define rep(i, n) for (int i = 0; (int)i < n; ++i)
-#define repa(i, a, n) for (int i = a; (int)(i) < (n); (++i))
-#define rrep(i, n) for (int i = (n - 1); (int)i >= 0; --i)
-#define rrepa(i, a, n) for (int i = a; (int)i >= 0; --i)
-#define EACH(x, a) for (auto& x : a)
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
 #define output(msg) cout << (msg) << '\n'
 #define die(msg)         \
   do {                   \
     cout << msg << endl; \
     exit(0);             \
   } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
 
-template <class A> void read(vector<A>& v);
-template <class A, size_t S> void read(array<A, S>& a);
-template <class T> void read(T& x) {
-  cin >> x;
-}
-void read(double& d) {
-  string t;
-  read(t);
-  d = stod(t);
-}
-void read(long double& d) {
-  string t;
-  read(t);
-  d = stold(t);
-}
-template <class H, class... T> void read(H& h, T&... t) {
-  read(h);
-  read(t...);
-}
-template <class A> void read(vector<A>& x) {
-  EACH(a, x) read(a);
-}
-template <class A, size_t S> void read(array<A, S>& x) {
-  EACH(a, x) read(a);
-}
 template <typename T> bool chmax(T& a, const T& b) {
   return ((a < b) ? (a = b, true) : (false));
 }
@@ -56,23 +30,26 @@ using llint = long long int;
 
 int main() {
   FastIO;
-  llint a, b, c;
-  read(a, b, c);
+  int a, b, c;
+  cin >> a >> b >> c;
 
-  bool is_ok{false};
-
-  repa(i, 1, 10000001) {
-    llint bro = i * a;
-
-    if (bro - b > 0) {
-      bro -= b;
+  vector<llint> ans;
+  for (llint n = 1; n <= 10000000; ++n) {
+    llint res = n * a;
+    if (res - b > 0) {
+      res -= b;
     }
 
-    if (bro == c) {
-      output(i);
-      is_ok = true;
+    if (res == c) {
+      ans.push_back(n);
     }
   }
 
-  if (!is_ok) output(-1);
+  if (ans.size() == 0) {
+    die(-1);
+  }
+
+  for (auto n : ans) {
+    output(n);
+  }
 }
