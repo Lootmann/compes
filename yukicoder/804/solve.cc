8813d2dd9070b0c741c5ab2a8c9322911e73aed5
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 19:05:50 +0900

    yuki 804

---
+++ yukicoder/804/solve.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  int ans{};
+  for (int i = 1; i <= 100; ++i) {
+    int veg = i;
+    int meat = i * c;
+
+    if (veg <= a && meat <= b && veg + meat <= d) {
+      ans = i;
+    }
+  }
+  out(ans);
+}
