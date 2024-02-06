f3f3c17d41994cfdc969d7cecabde9fdb69b6bd7
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 17:18:22 +0900

    yuki 975

---
+++ yukicoder/975/solve.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int x, n, m;
+  cin >> x >> n >> m;
+
+  bool has_mr{false}, has_max{false};
+
+  rep(_, n) {
+    int a;
+    cin >> a;
+    if (a == x) {
+      has_mr = true;
+    }
+  }
+
+  rep(_, m) {
+    int b;
+    cin >> b;
+    if (b == x) {
+      has_max = true;
+    }
+  }
+
+  if (has_mr && has_max) {
+    out("MrMaxValu");
+  } else if (has_mr) {
+    out("MrMax");
+  } else if (has_max) {
+    out("MaxValu");
+  } else {
+    out(-1);
+  }
+}
