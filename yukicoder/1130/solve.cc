9a0dc26bf93cc42d44d3919816a354bd694f878d
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 15:40:48 +0900

    yuki 1130

---
+++ yukicoder/1130/solve.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int H, W;
+  cin >> H >> W;
+
+  vector<int> ai(H * W);
+  rep(i, H * W) cin >> ai[i];
+
+  sort(all(ai));
+
+  for (int i = 1; i <= H * W; ++i) {
+    cout << ai[i - 1];
+    cout << (i != 0 && i % W == 0 ? '\n' : ' ');
+  }
+}
