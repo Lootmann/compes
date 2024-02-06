c67baab92abb606158d85836c935dc373c825fb6
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 19:42:57 +0900

    yuki 841

---
+++ yukicoder/841/solve.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  string s, t;
+  cin >> s >> t;
+
+  string sat{"Sat"}, sun{"Sun"};
+
+  if ((s == sat || s == sun) && (t == sat || t == sun)) {
+    die("8/33");
+  }
+
+  if (s == sat || s == sun) {
+    die("8/32");
+  }
+
+  out("8/31");
+}
