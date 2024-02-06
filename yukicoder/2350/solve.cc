c3887080c90765c85d41ce2f36d3b7294313046d
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 09:53:26 +0900

    yuki 2350

--- yukicoder/2350/solve.cc
+++ yukicoder/2350/solve.cc
@@ -9,10 +9,6 @@ using namespace std;
 
 #define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
 #define rep(i, n) for (int i = 0; (int)i < n; ++i)
-#define repa(i, a, n) for (int i = a; (int)(i) < (n); (++i))
-#define rrep(i, n) for (int i = (n - 1); (int)i >= 0; --i)
-#define rrepa(i, a, n) for (int i = a; (int)i >= 0; --i)
-#define EACH(x, a) for (auto& x : a)
 #define output(msg) cout << (msg) << '\n'
 #define die(msg)         \
   do {                   \
@@ -20,31 +16,6 @@ using namespace std;
     exit(0);             \
   } while (0)
 
-template <class A> void read(vector<A>& v);
-template <class A, size_t S> void read(array<A, S>& a);
-template <class T> void read(T& x) {
-  cin >> x;
-}
-void read(double& d) {
-  string t;
-  read(t);
-  d = stod(t);
-}
-void read(long double& d) {
-  string t;
-  read(t);
-  d = stold(t);
-}
-template <class H, class... T> void read(H& h, T&... t) {
-  read(h);
-  read(t...);
-}
-template <class A> void read(vector<A>& x) {
-  EACH(a, x) read(a);
-}
-template <class A, size_t S> void read(array<A, S>& x) {
-  EACH(a, x) read(a);
-}
 template <typename T> bool chmax(T& a, const T& b) {
   return ((a < b) ? (a = b, true) : (false));
 }
@@ -56,9 +27,9 @@ using llint = long long int;
 
 int main() {
   FastIO;
-
   int x;
-  read(x);
+  cin >> x;
+
   if (3 <= x && x <= 5)
     output("spring");
   else if (6 <= x && x <= 8)
