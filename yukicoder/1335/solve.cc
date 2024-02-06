34eb2205ec129f5187f03f86fd8e1ef7f3ad36f3
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 14:13:02 +0900

    yuki 1335

---
+++ yukicoder/1335/solve.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  string a, s;
+  cin >> a >> s;
+
+  rep(i, (int)s.size()) {
+    if ('0' <= s[i] && s[i] <= '9') {
+      int idx = s[i] - '0';
+      cout << a[idx];
+    } else {
+      cout << s[i];
+    }
+  }
+  cout << endl;
+}
