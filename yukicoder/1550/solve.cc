9607066ce6c803d4ed5f8a9d14257af23a7daab2
Lootmann <chore.machines.10260930@gmail.com>
2024-02-06 13:09:18 +0900

    yuki 1550

---
+++ yukicoder/1550/solve.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define output(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ullint = unsigned long long int;
+
+int main() {
+  FastIO;
+  ullint n;
+  cin >> n;
+  output(n % 1000000007);
+}
