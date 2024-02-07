8aaa1545ea2695241394c8b3c2e4d14f6dd071e5
Lootmann <chore.machines.10260930@gmail.com>
2024-02-07 11:05:26 +0900

    yuki 89

---
+++ yukicoder/89/solve.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  long double c, Rin, Rout;
+  cin >> c >> Rin >> Rout;
+
+  long double r = (Rout - Rin) / 2;
+  long double R = r + Rin;
+  long double volume = 2 * M_PI * M_PI * r * r * R;
+
+  cout << fixed << setprecision(10);
+  out(volume * c);
+}
