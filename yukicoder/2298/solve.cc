c92d1f90aaeda398e01ad60d319e22fe0e70af69
Lootmann <chore.machines.10260930@gmail.com>
2024-02-07 14:21:24 +0900

    yuki 2298

--- yukicoder/2298/solve.cc
+++ yukicoder/2298/solve.cc
@@ -7,44 +7,21 @@ using namespace std;
 #define dump(...)
 #endif
 
+using llint = long long int;
+
 #define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
-#define rep(i, n) for (int i = 0; (int)i < n; ++i)
-#define repa(i, a, n) for (int i = a; (int)(i) < (n); (++i))
-#define rrep(i, n) for (int i = (n - 1); (int)i >= 0; --i)
-#define rrepa(i, a, n) for (int i = a; (int)i >= 0; --i)
-#define EACH(x, a) for (auto& x : a)
-#define output(msg) cout << (msg) << '\n'
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
 #define die(msg)         \
   do {                   \
     cout << msg << endl; \
     exit(0);             \
   } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
 
-template <class A> void read(vector<A>& v);
-template <class A, size_t S> void read(array<A, S>& a);
-template <class T> void read(T& x) {
-  cin >> x;
-}
-void read(double& d) {
-  string t;
-  read(t);
-  d = stod(t);
-}
-void read(long double& d) {
-  string t;
-  read(t);
-  d = stold(t);
-}
-template <class H, class... T> void read(H& h, T&... t) {
-  read(h);
-  read(t...);
-}
-template <class A> void read(vector<A>& x) {
-  EACH(a, x) read(a);
-}
-template <class A, size_t S> void read(array<A, S>& x) {
-  EACH(a, x) read(a);
-}
 template <typename T> bool chmax(T& a, const T& b) {
   return ((a < b) ? (a = b, true) : (false));
 }
@@ -52,45 +29,39 @@ template <typename T> bool chmin(T& a, const T& b) {
   return ((a > b) ? (a = b, true) : false);
 }
 
-using llint = long long int;
-
 int main() {
   FastIO;
-
   string s;
-  read(s);
+  cin >> s;
+
+  vector<int> ps;
 
   string yuki{"yukicoder"};
-  dump(yuki.size());
-  vector<int> found_pos;
-
   auto pos = s.find(yuki);
-
   if (pos == string::npos) die(0);
 
-  found_pos.push_back(pos);
-
+  ps.push_back(pos);
   while (pos != string::npos) {
-    dump(pos);
     pos = s.find(yuki, pos + yuki.size());
-    found_pos.push_back(pos);
+    if (pos != string::npos) ps.push_back(pos);
   }
 
-  dump(found_pos);
+  int ans{1};
+  if (ps.size() == 1) die(ans);
 
-  int ans{1}, series{};
-  rep(i, (int)found_pos.size() - 1) {
-    if (found_pos[i] + 9 == found_pos[i + 1]) {
-      if (series == 0) {
-        series += 2;
+  int cnt{};
+  rep(i, (int)ps.size() - 1) {
+    if (ps[i + 1] - ps[i] == 9) {
+      if (cnt == 0) {
+        cnt = 2;
       } else {
-        series++;
+        cnt++;
       }
     } else {
-      series = 0;
+      cnt = 0;
     }
-
-    chmax(ans, series);
+    chmax(ans, cnt);
   }
-  output(ans);
+
+  out(ans);
 }
