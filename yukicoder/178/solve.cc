882ce373abfcaa518bb002daddc0cf2709308540
Lootmann <chore.machines.10260930@gmail.com>
2024-02-07 11:17:19 +0900

    yuki 178

---
+++ yukicoder/178/solve.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint = long long int;
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  llint max_space{};
+  vector<llint> spaces;
+  rep(_, n) {
+    llint a, b;
+    cin >> a >> b;
+    llint space = a + 4 * b;
+    spaces.push_back(space);
+    chmax(max_space, space);
+  }
+
+  dump(max_space);
+  dump(spaces);
+
+  llint ans{};
+  rep(i, n) {
+    llint diff = max_space - spaces[i];
+    if (diff % 2 != 0) die(-1);
+    ans += diff / 2;
+  }
+  out(ans);
+}
