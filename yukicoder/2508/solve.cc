a1ccd3c94161184dfa85a3ad616e94b9fe51f390
Lootmann <chore.machines.10260930@gmail.com>
2024-02-07 11:40:00 +0900

    yuki 2508

--- yukicoder/2508/solve.cc
+++ yukicoder/2508/solve.cc
@@ -9,13 +9,14 @@ using namespace std;
 
 #define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define output(msg) cout << (msg) << '\n'
+#define out(msg) cout << (msg) << '\n'
 #define die(msg)         \
   do {                   \
     cout << msg << endl; \
     exit(0);             \
   } while (0)
 #define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
 #define INFi 1 << 30
 #define INFll 1LL << 60
 
@@ -28,10 +29,26 @@ template <typename T> bool chmin(T& a, const T& b) {
 
 using llint = long long int;
 
+/*
+ * a(x - p)(x - q)  = axx + bx + c
+ *
+ * b = -a(p+q)
+ * c = apq
+ *
+ * bb > 4ac <-> bb - 4ac > 0
+ * aa(pp + 2pq + qq) - 4aapq > 0
+ * pp - 2pq + qq > 0
+ * (p - q)(p - q) > 0
+ *
+ * p == q のときのみ bb > 4ac が成り立たない
+ * */
 int main() {
   FastIO;
   string a, p, q;
   cin >> a >> p >> q;
 
-  output(p == q ? "No" : "Yes");
+  if (p == q)
+    out("No");
+  else
+    out("Yes");
 }
