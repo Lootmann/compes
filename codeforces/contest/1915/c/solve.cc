efdf6e275603a9dcc872a2890284d82b7c6b36e3
Lootmann <chore.machines.10260930@gmail.com>
2024-02-07 16:51:27 +0900

    cf 1915 c

---
+++ codeforces/contest/1915/c/solve.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+bool is_square(llint n) {
+  llint l = (llint)sqrt(n);
+  return n == (llint)(l * l);
+}
+
+void solve() {
+  int n;
+  cin >> n;
+
+  llint sum{};
+  rep(_, n) {
+    llint a;
+    cin >> a;
+    sum += a;
+  }
+
+  if (is_square(sum)) {
+    out("YES");
+  } else {
+    out("NO");
+  }
+}
+
+int main() {
+  FastIO;
+
+  int t;
+  cin >> t;
+  while (t--) solve();
+}
