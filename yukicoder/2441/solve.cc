daa4ca4b4db96f96a2da0fe0a186abecebd01f7f
Lootmann <chore.machines.10260930@gmail.com>
2024-02-07 09:32:51 +0900

    2441

--- yukicoder/2441/solve.cc
+++ yukicoder/2441/solve.cc
@@ -8,43 +8,18 @@ using namespace std;
 #endif
 
 #define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
-#define rep(i, n) for (int i = 0; (int)i < n; ++i)
-#define repa(i, a, n) for (int i = a; (int)(i) < (n); (++i))
-#define rrep(i, n) for (int i = (n - 1); (int)i >= 0; --i)
-#define rrepa(i, a, n) for (int i = a; (int)i >= 0; --i)
-#define EACH(x, a) for (auto& x : a)
-#define output(msg) cout << msg << '\n'
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
 #define die(msg)         \
   do {                   \
     cout << msg << endl; \
     exit(0);             \
   } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
 
-template <class A> void read(vector<A>& v);
-template <class A, size_t S> void read(array<A, S>& a);
-template <class T> void read(T& x) {
-  cin >> x;
-}
-void read(double& d) {
-  string t;
-  read(t);
-  d = stod(t);
-}
-void read(long double& d) {
-  string t;
-  read(t);
-  d = stold(t);
-}
-template <class H, class... T> void read(H& h, T&... t) {
-  read(h);
-  read(t...);
-}
-template <class A> void read(vector<A>& x) {
-  EACH(a, x) read(a);
-}
-template <class A, size_t S> void read(array<A, S>& x) {
-  EACH(a, x) read(a);
-}
 template <typename T> bool chmax(T& a, const T& b) {
   return ((a < b) ? (a = b, true) : (false));
 }
@@ -53,24 +28,34 @@ template <typename T> bool chmin(T& a, const T& b) {
 }
 
 using llint = long long int;
-using VI = vector<int>;
 
-// ?
-VI mul(const VI A, const VI B) {
-  VI C(4, 0);
-  C[0] = A[0] * B[0] + A[1] * B[2], C[1] = A[0] * B[1] + A[1] * B[3];
-  C[2] = A[2] * B[0] + A[3] * B[2], C[3] = A[2] * B[1] + A[3] * B[3];
-  return C;
+using vvi = vector<vector<int>>;
+using vi = vector<int>;
+
+vvi mul(vvi a, vvi b) {
+  vvi res(2, vi(2, 0));
+
+  rep(i, 2) {
+    rep(j, 2) {
+      rep(k, 2) {
+        res[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+
+  return res;
 }
 
 int main() {
   FastIO;
+  vvi mat(2, vi(2, 0));
 
-  vector<int> vs(4, 0);
-  read(vs);
+  rep(i, 2) rep(j, 2) {
+    cin >> mat[i][j];
+  }
 
-  // M^3 == M^2 * M
-  VI ans = mul(mul(vs, vs), vs);
-  cout << ans[0] << ' ' << ans[1] << '\n';
-  cout << ans[2] << ' ' << ans[3] << '\n';
+  vvi res = mul(mul(mat, mat), mat);
+
+  cout << res[0][0] << ' ' << res[0][1] << '\n';
+  cout << res[1][0] << ' ' << res[1][1] << '\n';
 }
