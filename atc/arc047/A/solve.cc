299fc7efba6f93ee3badf201ffe37f04190e4eca
Lootmann <chore.machines.10260930@gmail.com>
2024-02-09 16:01:52 +0900

    arc047 a

---
+++ atc/arc047/A/solve.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+int main() {
+  FastIO;
+  int n, l;
+  string history;
+  cin >> n >> l >> history;
+
+  int tabs{1}, crush{};
+
+  for (auto his : history) {
+    if (his == '+') {
+      tabs++;
+
+      if (tabs > l) {
+        crush++;
+        tabs = 1;
+      }
+    } else {
+      tabs--;
+    }
+  }
+
+  out(crush);
+}
