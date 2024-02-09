5b5b92c30c009389f2731a95f9b7fd628242d2b2
Lootmann <chore.machines.10260930@gmail.com>
2024-02-09 11:32:16 +0900

    arc012 a

---
+++ atc/arc012/A/solve.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+int main() {
+  FastIO;
+  string day;
+  cin >> day;
+
+  if (day == "Sunday" || day == "Saturday") die(0);
+  if (day == "Monday") die(5);
+  if (day == "Tuesday") die(4);
+  if (day == "Wednesday") die(3);
+  if (day == "Thursday") die(2);
+  out(1);
+}
