2dfd68c1538102396a4d5695de82f8a4f3c40d68
Lootmann <chore.machines.10260930@gmail.com>
2024-02-09 20:00:29 +0900

    abc002 b

---
+++ atc/abc002/B/solve.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+bool is_vowel(char ch) {
+  return ch == 'a' || ch == 'i' || ch == 'u' || ch == 'e' || ch == 'o';
+}
+
+int main() {
+  FastIO;
+  string s;
+  cin >> s;
+
+  for (auto ch : s) {
+    if (!is_vowel(ch)) {
+      cout << ch;
+    }
+  }
+  cout << endl;
+}
