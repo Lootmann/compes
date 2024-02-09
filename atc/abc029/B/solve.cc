3d3c73a11c43ab9011f349ea4fb9fe9f71aa5ec6
Lootmann <chore.machines.10260930@gmail.com>
2024-02-09 17:56:38 +0900

    abc029 b

---
+++ atc/abc029/B/solve.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+int main() {
+  FastIO;
+
+  int cnt{};
+  rep(_, 12) {
+    string s;
+    cin >> s;
+
+    bool has_r{false};
+    for (auto ch : s) {
+      if (ch == 'r') {
+        has_r = true;
+      }
+    }
+    if (has_r) cnt++;
+  }
+
+  out(cnt);
+}
