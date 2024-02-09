b38db7fa4ac33b51498a79c6153a7f267fca375f
Lootmann <chore.machines.10260930@gmail.com>
2024-02-09 17:52:49 +0900

    VC Private 3

---
+++ atc/arc110/A/solve.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+int main() {
+  FastIO;
+  llint n;
+  cin >> n;
+
+  llint ans{1};
+  for (llint i = 2; i <= n; ++i) {
+    ans = lcm(ans, i);
+    dump(ans);
+  }
+
+  out(ans + 1);
+}
