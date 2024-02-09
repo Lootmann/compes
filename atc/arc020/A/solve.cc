46e1c1f39ad27c947ff4ee4813f669dd577b47e0
Lootmann <chore.machines.10260930@gmail.com>
2024-02-09 13:59:19 +0900

    arc020 a

---
+++ atc/arc020/A/solve.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+int main() {
+  FastIO;
+  int a, b;
+  cin >> a >> b;
+
+  a = abs(a), b = abs(b);
+  if (a > b) {
+    out("Bug");
+  } else if (a == b) {
+    out("Draw");
+  } else {
+    out("Ant");
+  }
+}
