84786ddc32794b99a707c3029d3b7fbd2e3c8459
Lootmann <chore.machines.10260930@gmail.com>
2024-02-09 19:42:20 +0900

    arc112 a

---
+++ atc/arc112/A/solve.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+void solve() {
+  llint left, right;
+  cin >> left >> right;
+
+  llint lim = right - 2 * left;
+  if (lim < 0) {
+    out(0);
+    return;
+  }
+
+  out((lim + 1) * (lim + 2) / 2);
+}
+
+int main() {
+  FastIO;
+  int t;
+  cin >> t;
+  while (t--) solve();
+}
