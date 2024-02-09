dac60df4b3ba954466bcc792e8481b817c05229d
Lootmann <chore.machines.10260930@gmail.com>
2024-02-09 11:48:38 +0900

    abc030 b

---
+++ atc/abc030/A/solve.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+int main() {
+  FastIO;
+  double a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  double tak = b / a;
+  double aoki = d / c;
+  dump(tak, aoki);
+
+  if (tak == aoki) {
+    out("DRAW");
+  } else if (tak > aoki) {
+    out("TAKAHASHI");
+  } else {
+    out("AOKI");
+  }
+}
