37540b14986869db504f5884f8e81625d0d3bf19
Lootmann <chore.machines.10260930@gmail.com>
2024-02-10 20:58:31 +0900

    abc182 abc

---
+++ atc/abc182/C/solve.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  string s;
+  cin >> s;
+
+  int ans{-1};
+  int n = (int)s.size();
+  for (int bit = 0; bit < (1 << n); ++bit) {
+    string digit{};
+    for (int i = 0; i < n; ++i) {
+      if (bit & (1 << i)) {
+        digit.push_back(s[i]);
+      }
+    }
+
+    if (digit != "") {
+      llint num = stoll(digit);
+
+      if (num % 3 == 0) {
+        chmax(ans, (int)digit.size());
+      }
+    }
+  }
+
+  if (ans == -1)
+    out(ans);
+  else
+    out(n - ans);
+}
