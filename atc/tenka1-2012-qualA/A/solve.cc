b1473df89308a200a9199a12afb6972cd111775f
Lootmann <chore.machines.10260930@gmail.com>
2024-02-10 10:49:19 +0900

    tenka1 2012 quala a

---
+++ atc/tenka1-2012-qualA/A/solve.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+using llint = long long int;
+
+#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg)         \
+  do {                   \
+    cout << msg << endl; \
+    exit(0);             \
+  } while (0)
+#define all(k) k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi 1 << 30
+#define INFll 1LL << 60
+
+template <typename T> bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+int main() {
+  FastIO;
+  int n;
+  cin >> n;
+
+  vector<llint> memo(50, 0);
+  memo[0] = 1;
+  memo[1] = 1;
+  for (int i = 2; i < 50; ++i) {
+    memo[i] = memo[i - 1] + memo[i - 2];
+  }
+
+  out(memo[n]);
+}
