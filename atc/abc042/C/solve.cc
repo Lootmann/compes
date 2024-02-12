1c593a85b7c9c3edcea4f9a7acf5bc461c13906b
Lootmann <chore.machines.10260930@gmail.com>
2024-02-12 22:56:21 +0900

    abc042 abc

---
+++ atc/abc042/C/solve.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+bool is_include(const set<int>& st, int k) {
+  while (k > 0) {
+    if (st.find(k % 10) != st.end()) return true;
+    k /= 10;
+  }
+  return false;
+}
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  set<int> di;
+  rep(i, k) {
+    int d;
+    cin >> d;
+    di.insert(d);
+  }
+
+  int ans{n};
+  while (true) {
+    if (!is_include(di, ans)) {
+      break;
+    }
+
+    ans++;
+  }
+  out(ans);
+}
