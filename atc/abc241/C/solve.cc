31dfc5b72574893ba72f0e4fc4508f44ff2821dd
Lootmann <chore.machines.10260930@gmail.com>
2024-02-12 17:25:39 +0900

    abc241 abc

---
+++ atc/abc241/C/solve.cc
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<string> grid(n);
+  rep(i, n) cin >> grid[i];
+
+  int H = n, W = (int)grid[0].size();
+  bool can_draw{false};
+
+  // horizontal
+  rep(h, H) rep(w, W - 5) {
+    int cnt{};
+
+    rep(k, 6) {
+      if (grid[h][w + k] == '#') cnt++;
+    }
+    if (cnt >= 4) can_draw = true;
+  }
+
+  // vertical
+  rep(w, W) rep(h, H - 5) {
+    int cnt{};
+
+    rep(k, 6) {
+      if (grid[h + k][w] == '#') cnt++;
+    }
+    if (cnt >= 4) can_draw = true;
+  }
+
+  // diagonal
+  rep(h, H - 5) rep(w, W - 5) {
+    {
+      // from left-top to right-bottom
+      int cnt{};
+      rep(k, 6) {
+        if (grid[h + k][w + k] == '#') cnt++;
+      }
+      if (cnt >= 4) can_draw = true;
+    }
+
+    {
+      // from left-top to right-top
+      int cnt = 0;
+      rep(k, 6) {
+        if (grid[h + 5 - k][w + k] == '#') cnt++;
+      }
+      if (cnt >= 4) can_draw = true;
+    }
+  }
+
+  // output
+  out(can_draw ? "Yes" : "No");
+}
