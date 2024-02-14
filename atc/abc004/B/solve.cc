a59f09aae391e77700ff57b855f905b601f0235b
Lootmann <chore.machines.10260930@gmail.com>
2024-02-14 11:54:39 +0900

    abc004 bc

---
+++ atc/abc004/B/solve.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  const int N = 4;
+
+  vector<vector<char>> grid(N, vector<char>(N));
+  rep(i, N) rep(j, N) cin >> grid[i][j];
+
+  rep(i, 2) {
+    rep(w, N) {
+      char ch = grid[i][w];
+      grid[i][w] = grid[N - i - 1][N - w - 1];
+      grid[N - i - 1][N - w - 1] = ch;
+    }
+  }
+
+  rep(i, N) {
+    rep(j, N) {
+      cout << grid[i][j];
+      cout << (j == N - 1 ? '\n' : ' ');
+    }
+  }
+}
