bb115cd8ba4c2d220310bc68156bd663d8eff3b8
Lootmann <chore.machines.10260930@gmail.com>
2024-02-14 21:48:28 +0900

    abc089 c

--- atc/abc089/C/solve.cc
+++ atc/abc089/C/solve.cc
@@ -7,50 +7,67 @@ using namespace std;
 #define dump(...)
 #endif
 
-#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
-#define rep(i, n) for (int i = 0; (int)i < n; ++i)
-#define output(msg) cout << (msg) << '\n'
-#define die(msg)         \
-  do {                   \
-    cout << msg << endl; \
-    exit(0);             \
-  } while (0)
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
 
-template <typename T> bool chmax(T& a, const T& b) {
-  return ((a < b) ? (a = b, true) : (false));
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = unsigned long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
 }
-template <typename T> bool chmin(T& a, const T& b) {
+template <typename T> inline bool chmin(T& a, const T& b) {
   return ((a > b) ? (a = b, true) : false);
 }
-
-using llint = long long int;
+// clang-format on
 
 int main() {
-  FastIO;
   int n;
   cin >> n;
 
-  // m a r c h
-  vector<llint> names(5, 0);
-  rep(_, n) {
+  vector<llint> vi(5, 0);
+  rep(i, n) {
     string s;
     cin >> s;
-    if (s[0] == 'M') names[0]++;
-    if (s[0] == 'A') names[1]++;
-    if (s[0] == 'R') names[2]++;
-    if (s[0] == 'C') names[3]++;
-    if (s[0] == 'H') names[4]++;
+
+    if (s[0] == 'M')
+      vi[0]++;
+    else if (s[0] == 'A')
+      vi[1]++;
+    else if (s[0] == 'R')
+      vi[2]++;
+    else if (s[0] == 'C')
+      vi[3]++;
+    else if (s[0] == 'H')
+      vi[4]++;
   }
 
-  dump(names);
+  int c{};
+  rep(i, 5) {
+    if (vi[i] >= 1) c++;
+  }
+  if (c < 3) die(0);
 
-  llint cnt{0};
+  llint cnt{};
   for (int i = 0; i < 5; ++i) {
     for (int j = i + 1; j < 5; ++j) {
       for (int k = j + 1; k < 5; ++k) {
-        cnt += names[i] * names[j] * names[k];
+        cnt += vi[i] * vi[j] * vi[k];
       }
     }
   }
-  output(cnt);
+  out(cnt);
 }
