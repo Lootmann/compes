28311b22d7150dfdc1d982db69a47cf118dfc90d
Lootmann <chore.machines.10260930@gmail.com>
2024-02-14 15:34:48 +0900

    abc299 abc

---
+++ atc/abc299/B/solve.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n, t;
+  cin >> n >> t;
+
+  vector<int> ci(n), ri(n);
+  rep(i, n) cin >> ci[i];
+  rep(i, n) cin >> ri[i];
+
+  {
+    dump("first");
+    int max_val{}, ans_idx{-1};
+    rep(i, n) {
+      if (ci[i] == t) {
+        if (chmax(max_val, ri[i])) {
+          ans_idx = i;
+        }
+      }
+    }
+
+    if (ans_idx != -1) {
+      die(ans_idx + 1);
+    }
+  }
+
+  {
+    dump("second");
+    int color_one = ci[0];
+    int max_val = ri[0];
+    int ans_idx{0};
+
+    rep(i, n) {
+      if (ci[i] == color_one) {
+        if (chmax(max_val, ri[i])) {
+          ans_idx = i;
+        }
+      }
+    }
+
+    out(ans_idx + 1);
+  }
+}
