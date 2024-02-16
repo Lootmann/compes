a61e1c1119e59167a437ec2904f979fb126e61b8
Lootmann <chore.machines.10260930@gmail.com>
2024-02-16 13:34:40 +0900

    abc225 abc

---
+++ atc/abc225/C/solve.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+#define el '\n'
+
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<int>> grid(n, vector<int>(m, 0));
+  rep(i, n) rep(j, m) cin >> grid[i][j];
+
+  rep(h, n) rep(w, m - 1) {
+    if (grid[h][w] + 1 != grid[h][w + 1]) {
+      die("No");
+    }
+  }
+
+  rep(h, n) rep(w, m - 1) {
+    if (grid[h][w] % 7 == 0) {
+      die("No");
+    }
+  }
+
+  rep(h, n) rep(w, m) {
+    if (h > 0) {
+      if (grid[h][w] != grid[h - 1][w] + 7) {
+        die("No");
+      }
+    }
+  }
+
+  out("Yes");
+}
