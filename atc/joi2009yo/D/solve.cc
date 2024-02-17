70a025e21791fe2a470875aa895d068d4afe206e
Lootmann <chore.machines.10260930@gmail.com>
2024-02-17 15:27:35 +0900

    joi2009yo d

--- atc/joi2009yo/D/solve.cc
+++ atc/joi2009yo/D/solve.cc
@@ -7,65 +7,72 @@ using namespace std;
 #define dump(...)
 #endif
 
-#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define output(msg) cout << (msg) << '\n'
-#define die(msg)         \
-  do {                   \
-    cout << msg << endl; \
-    exit(0);             \
-  } while (0)
-#define all(k) k.begin(), k.end()
-#define INFi 1 << 30
-#define INFll 1LL << 60
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+#define el '\n'
 
-template <typename T> bool chmax(T& a, const T& b) {
-  return ((a < b) ? (a = b, true) : (false));
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
 }
-template <typename T> bool chmin(T& a, const T& b) {
+template <typename T> inline bool chmin(T& a, const T& b) {
   return ((a > b) ? (a = b, true) : false);
 }
+// clang-format on
 
-using llint = long long int;
 using Graph = vector<vector<int>>;
+using vvb = vector<vector<bool>>;
 
-int dy[]{0, -1, 1, 0};
-int dx[]{-1, 0, 0, 1};
-int max_depth{0};
-int N, M;
-
-bool is_inbound(int y, int x) {
-  return 0 <= y && y < N && 0 <= x && x < M;
-}
-
-void dfs(Graph& graph, const int y, const int x, int depth) {
-  graph[y][x] = 0;
-
-  rep(k, 4) {
-    int ny = y + dy[k];
-    int nx = x + dx[k];
-
-    if (!is_inbound(ny, nx)) continue;
-    if (graph[ny][nx] != 1) continue;
-    dfs(graph, ny, nx, depth + 1);
-  }
-
-  graph[y][x] = 1;
-  chmax(max_depth, depth);
-}
+int dh[]{-1, 0, 0, 1};
+int dw[]{0, -1, 1, 0};
 
 int main() {
-  FastIO;
-  cin >> N >> M;
+  int H, W;
+  cin >> W >> H;
 
-  Graph graph(N, vector<int>(M, 0));
-  rep(y, N) rep(x, M) cin >> graph[y][x];
+  Graph graph(H, vector<int>(W, 0));
+  rep(h, H) rep(w, W) cin >> graph[h][w];
 
-  rep(y, N) rep(x, M) {
-    if (graph[y][x] == 1) {
-      dfs(graph, y, x, 1);
+  vvb visited(H, vector<bool>(W, false));
+
+  int max_depth{};
+
+  auto dfs = [&](auto f, int h, int w, int depth) -> void {
+    graph[h][w] = 0;
+
+    rep(k, 4) {
+      int nh = h + dh[k];
+      int nw = w + dw[k];
+
+      if (!(0 <= nh && nh < H && 0 <= nw && nw < W)) continue;
+      if (graph[nh][nw] == 0) continue;
+
+      f(f, nh, nw, depth + 1);
+    }
+
+    graph[h][w] = 1;
+    chmax(max_depth, depth);
+  };
+
+  rep(h, H) rep(w, W) {
+    if (graph[h][w] == 1) {
+      dfs(dfs, h, w, 1);
     }
   }
-
-  output(max_depth);
+  out(max_depth);
 }
