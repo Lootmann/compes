00126f4b0b2c78ee0b090ff7a1fb7ab8c1bd22c5
Lootmann <chore.machines.10260930@gmail.com>
2024-02-17 09:23:27 +0900

    abc253 ab

---
+++ atc/abc253/B/solve.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+#define el '\n'
+
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int H, W;
+  cin >> H >> W;
+
+  vector<string> vs(H);
+  rep(i, H) cin >> vs[i];
+
+  vector<pair<int, int>> vp;
+
+  rep(h, H) rep(w, W) {
+    if (vs[h][w] == 'o') {
+      vp.push_back({h, w});
+    }
+  }
+
+  auto [x1, y1] = vp.front();
+  auto [x2, y2] = vp.back();
+  out(abs(x1 - x2) + abs(y1 - y2));
+}
