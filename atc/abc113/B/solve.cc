916dea6ef6f0d6c22a193d4407c161b13e81948c
Lootmann <chore.machines.10260930@gmail.com>
2024-02-20 02:57:49 +0900

    abc113 abc

---
+++ atc/abc113/B/solve.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+#define el '\n'
+
+#define all(k)  k.begin(), k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+
+  double t, a;
+  cin >> t >> a;
+
+  vector<double> hi(n);
+  rep(i, n) cin >> hi[i];
+
+  int ans{};
+  double cur{999999.0};
+
+  rep(i, n) {
+    double ave_temp = t - hi[i] * 0.006;
+    if (abs(ave_temp - a) < cur) {
+      cur = abs(ave_temp - a);
+      ans = i + 1;
+    }
+    dump(ave_temp, cur);
+  }
+
+  cout << ans << el;
+}
