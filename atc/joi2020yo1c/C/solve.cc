b0fd74df151b7be4be94755fcfd1c21eda773b1c
Lootmann <chore.machines.10260930@gmail.com>
2024-02-23 08:40:50 +0900

    joi2020yo1c c

---
+++ atc/joi2020yo1c/C/solve.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> ai(n);
+  rep(i, n) cin >> ai[i];
+
+  int ans{1}, cnt{0};
+  rep(i, n - 1) {
+    if (ai[i] <= ai[i + 1]) {
+      if (cnt == 0)
+        cnt = 2;
+      else
+        cnt++;
+    } else {
+      cnt = 0;
+    }
+
+    dump(ans, cnt);
+    chmax(ans, cnt);
+  }
+
+  cout << ans << el;
+}
