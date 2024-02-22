8923a9bcdc699aee56a75abc6e1e8e765019cf61
Lootmann <chore.machines.10260930@gmail.com>
2024-02-22 21:35:27 +0900

    joi2023yo1a abcd

---
+++ atc/joi2023yo1a/C/solve.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define out(msg) cout << msg << '\n'
+#define die(msg) do{ cout << msg << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+
+  vector<int> box(3, 0);
+  int idx{}, cnt{};
+
+  for (auto ch : s) {
+    if (ch == 'L') {
+      if (0 < idx) {
+        box[idx] = 0;
+        idx--;
+        box[idx] = 1;
+      }
+    } else {
+      if (idx < 2) {
+        box[idx] = 0;
+        idx++;
+        box[idx] = 1;
+      }
+    }
+
+    if (box[2] == 1) {
+      cnt++;
+    }
+  }
+
+  cout << cnt << el;
+}
