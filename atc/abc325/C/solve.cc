e90d4d34994ed8183122854ab1848d5b3e384b67
Lootmann <chore.machines.10260930@gmail.com>
2024-02-26 05:49:44 +0900

    abc325 c

--- atc/abc325/C/solve.cc
+++ atc/abc325/C/solve.cc
@@ -8,12 +8,14 @@ using namespace std;
 #endif
 
 // clang-format off
-struct  Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define out(msg) cout << (msg) << '\n'
-#define die(msg) do {cout << msg << endl;exit(0);} while (0)
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
-#define all(k)  k.begin(), k.end()
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
 #define rall(k) k.rbegin(), k.rend()
 
 // const
@@ -34,59 +36,50 @@ template <typename T> inline bool chmin(T& a, const T& b) {
 }
 // clang-format on
 
-using vvb = vector<vector<bool>>;
-using vs = vector<string>;
-using ci = const int;
-
 int dh[]{-1, 0, 1, -1, 1, -1, 0, 1};
 int dw[]{-1, -1, -1, 0, 0, 1, 1, 1};
 
-using P = pair<int, int>;
-
-int H, W;
-bool is_inbound(int h, int w) {
-  return 0 <= h && h < H && 0 <= w && w < W;
-}
-
-void dfs(const vs& grid, vvb& visited, ci start_h, ci start_w) {
-  visited[start_h][start_w] = true;
-
-  stack<P> st;
-  st.push({start_h, start_w});
-
-  while (!st.empty()) {
-    auto [h, w] = st.top();
-    st.pop();
-
-    rep(k, 8) {
-      int nh = h + dh[k];
-      int nw = w + dw[k];
-
-      if (!is_inbound(nh, nw)) continue;
-      if (grid[nh][nw] != '#') continue;
-      if (visited[nh][nw]) continue;
-
-      visited[nh][nw] = true;
-      st.push({nh, nw});
-    }
-  }
-}
-
 int main() {
+  int H, W;
   cin >> H >> W;
 
   vector<string> grid(H);
-  rep(i, H) cin >> grid[i];
+  rep(h, H) cin >> grid[h];
 
-  vvb visited(H, vector<bool>(W, 0));
+  vector<vector<int>> dist(H, vector<int>(W, 0));
 
-  int id{};
+  auto dfs = [&](int sh, int sw, int id) {
+    dist[sh][sw] = id;
+
+    stack<pair<int, int>> st;
+    st.push({sh, sw});
+
+    while (!st.empty()) {
+      auto [h, w] = st.top();
+      st.pop();
+
+      rep(k, 8) {
+        int nh = h + dh[k];
+        int nw = w + dw[k];
+
+        if (!(0 <= nh && nh < H && 0 <= nw && nw < W)) continue;
+        if (grid[nh][nw] != '#') continue;
+        if (dist[nh][nw] != 0) continue;
+
+        st.push({nh, nw});
+        dist[nh][nw] = id;
+      }
+    }
+  };
+
+  int id{1};
   rep(h, H) rep(w, W) {
-    if (visited[h][w]) continue;
-    if (grid[h][w] != '#') continue;
-
-    dfs(grid, visited, h, w);
-    id++;
+    if (grid[h][w] == '#' && dist[h][w] == 0) {
+      dfs(h, w, id++);
+    }
   }
-  out(id);
+
+  int ans{};
+  rep(h, H) rep(w, W) chmax(ans, dist[h][w]);
+  cout << ans << el;
 }
