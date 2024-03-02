c4315dfcf816382a96dfa3143342f84692136237
Lootmann <chore.machines.10260930@gmail.com>
2024-03-03 03:15:45 +0900

    aoj alds1_9_b

--- aoj/solve.cc
+++ aoj/solve.cc
@@ -7,95 +7,75 @@ using namespace std;
 #define dump(...)
 #endif
 
-#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define output(msg) cout << (msg) << '\n'
-#define die(msg)         \
-  do {                   \
-    cout << msg << endl; \
-    exit(0);             \
-  } while (0)
-#define all(k) k.begin(), k.end()
-#define INFi 1 << 30
-#define INFll 1LL << 60
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
-template <typename T> bool chmax(T& a, const T& b) {
-  return ((a < b) ? (a = b, true) : (false));
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
 }
-template <typename T> bool chmin(T& a, const T& b) {
+template <typename T> inline bool chmin(T& a, const T& b) {
   return ((a > b) ? (a = b, true) : false);
 }
+// clang-format on
 
-using llint = long long int;
-using Graph = vector<vector<int>>;
-using VVI = vector<vector<int>>;
+void maxHeapify(vector<int>& ai, int N, int i) {
+  int left = 2 * i;
+  int right = 2 * i + 1;
+  int largest{};
 
-int dy[]{-1, 0, 1, -1, 1, -1, 0, 1};
-int dx[]{-1, -1, -1, 0, 0, 1, 1, 1};
-
-bool is_inbound(int h, int w, int H, int W) {
-  return 0 <= h && h < H && 0 <= w && w < W;
-}
-
-struct Pos {
-  int y;
-  int x;
-  Pos(int _y, int _x) : y(_y), x(_x) {}
-};
-
-void bfs(const Graph& graph, VVI& visited, int h, int w, int H, int W, int id) {
-  visited[h][w] = id;
-
-  stack<Pos> st;
-  st.push(Pos(h, w));
-
-  while (!st.empty()) {
-    Pos v = st.top();
-    st.pop();
-
-    rep(k, 8) {
-      int nh = v.y + dy[k];
-      int nw = v.x + dx[k];
-      if (is_inbound(nh, nw, H, W)) {
-        if (graph[nh][nw] == 1 && visited[nh][nw] != id) {
-          visited[nh][nw] = id;
-          st.push(Pos(nh, nw));
-        }
-      }
-    }
-  }  // end bfs
-}
-
-void solve(const int W, const int H) {
-  Graph graph(H, vector<int>(W, 0));
-  rep(h, H) rep(w, W) cin >> graph[h][w];
-
-  VVI visited(H, vector<int>(W, false));
-
-  int id{1};
-  rep(h, H) rep(w, W) {
-    if (graph[h][w] == 1 && !visited[h][w]) {
-      bfs(graph, visited, h, w, H, W, id++);
-    }
+  if (left <= N && ai[left] > ai[i]) {
+    largest = left;
+  } else {
+    largest = i;
   }
 
-  cerr << "* bfs" << '\n';
-  rep(i, H) dump(visited[i]);
-
-  // answer
-  int ans{};
-  rep(h, H) rep(w, W) {
-    chmax(ans, visited[h][w]);
+  if (right <= N && ai[right] > ai[largest]) {
+    largest = right;
   }
-  output(ans);
+
+  if (largest != i) {
+    int t = ai[i];
+    ai[i] = ai[largest];
+    ai[largest] = t;
+    maxHeapify(ai, N, largest);
+  }
+}
+
+void buildMaxHeap(vector<int>& ai, const int N) {
+  for (int i = N / 2; i >= 1; --i) {
+    maxHeapify(ai, N, i);
+  }
 }
 
 int main() {
-  FastIO;
-  int W, H;
+  int n;
+  cin >> n;
 
-  while (cin >> W >> H) {
-    if (W == 0 && H == 0) break;
-    solve(W, H);
+  vector<int> heap(n + 1, 0);
+  rep(i, n) cin >> heap[i + 1];
+
+  buildMaxHeap(heap, n);
+
+  rep(i, n) {
+    cout << ' ' << heap[i + 1];
   }
+  cout << el;
 }
