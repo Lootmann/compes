5b5cad80c2d994b3353aac5126c61e26d9f7b319
Lootmann <chore.machines.10260930@gmail.com>
2024-03-07 08:13:16 +0900

    abc057 b

--- atc/abc057/B/solve.cc
+++ atc/abc057/B/solve.cc
@@ -7,60 +7,66 @@ using namespace std;
 #define dump(...)
 #endif
 
-#define FastIO cin.tie(nullptr), ios_base::sync_with_stdio(false);
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define output(msg) cout << (msg) << '\n'
-#define die(msg)         \
-  do {                   \
-    cout << msg << endl; \
-    exit(0);             \
-  } while (0)
-#define all(k) k.begin(), k.end()
-#define INFi 1 << 30
-#define INFll 1LL << 60
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
-template <typename T> bool chmax(T& a, const T& b) {
-  return ((a < b) ? (a = b, true) : (false));
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
 }
-template <typename T> bool chmin(T& a, const T& b) {
+template <typename T> inline bool chmin(T& a, const T& b) {
   return ((a > b) ? (a = b, true) : false);
 }
+// clang-format on
 
-using llint = long long int;
+using P = pair<int, int>;
 
 int main() {
-  FastIO;
-
   int n, m;
   cin >> n >> m;
 
-  vector<pair<int, int>> students(n), checkpoints(m);
-  rep(i, n) {
+  vector<P> students, checkpoints;
+  rep(_, n) {
     int a, b;
     cin >> a >> b;
-    students[i] = make_pair(a, b);
+    students.push_back({a, b});
   }
 
-  rep(i, m) {
+  rep(_, m) {
     int c, d;
     cin >> c >> d;
-    checkpoints[i] = make_pair(c, d);
+    checkpoints.push_back({c, d});
   }
 
-  rep(i, n) {
-    int min_dist{INFi}, ans{51};
+  for (auto [a, b] : students) {
+    int point{};
+    int min_dist{INFi};
 
-    pair<int, int> s = students[i];
-    rep(j, m) {
-      pair<int, int> p = checkpoints[j];
-      int dist = abs(s.first - p.first) + abs(s.second - p.second);
+    rep(i, m) {
+      auto [c, d] = checkpoints[i];
+      int cur = abs(a - c) + abs(b - d);
 
-      if (min_dist > dist) {
-        min_dist = dist;
-        ans = j + 1;
-        chmin(ans, j + 1);
+      if (chmin(min_dist, cur)) {
+        point = i;
       }
     }
-    output(ans);
+    cout << point + 1 << el;
   }
 }
